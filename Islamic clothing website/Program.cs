66205615a8086eae8daca51d6eb1093006b316b2
Muhammad Shaharyar <ac149815@avcol.school.nz>
2023-05-24 12:29:13 +1200

    added identity

--- Islamic clothing website/Program.cs
+++ Islamic clothing website/Program.cs
@@ -1,4 +1,14 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Islamic_clothing_website.Areas.Identity.Data;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("IslamicClothingContextDbConnection") ?? throw new InvalidOperationException("Connection string 'IslamicClothingContextDbConnection' not found.");
+
+builder.Services.AddDbContext<IslamicClothingContextDb>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<IslamicClothingUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<IslamicClothingContextDb>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -17,11 +27,14 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();;
 
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
 
 app.Run();
