52643ed4fb21afeb869934323d67cf990ff97d9c
Muhammad Shaharyar <ac149815@avcol.school.nz>
2023-08-07 15:19:42 +1200

    phone no changed

---
+++ Islamic clothing website/Migrations/20230807025428_changephoneno.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Islamic_clothing_website.Migrations
+{
+    public partial class changephoneno : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "Customer",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "PhoneNumber",
+                table: "Customer",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
--- Islamic clothing website/Models/Customer.cs
+++ Islamic clothing website/Models/Customer.cs
@@ -9,7 +9,7 @@ namespace Islamic_clothing_website.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
         
-        public int PhoneNumber { get; set; }
+        public string PhoneNumber { get; set; }
         public string Email { get; set; }
     }
 }
